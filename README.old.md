c62300a4f6316513d321d53bb0784c2421ad61b8
sameer-j <samir4nov@gmail.com>
2021-10-28 21:34:41 +0530

    structure setup - NOT RUNNABLE

---
+++ README.old.md
@@ -0,0 +1,5 @@
+# React Chat App
+React based chat app
+
+
+Based on [Understanding Redux](https://thereduxjsbooks.com/) by @ohansemmanuel
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Sidebar from "./Sidebar";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Sidebar />
+      {/* <Main /> */}
+    </div>
+  );
+};
